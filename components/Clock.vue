3669842ef443f9accc88d4e0dc175ef0f88f25a6
Nojusle <nojusel@gmail.com>
2020-02-23 19:44:50 +0200

    rferg

---
+++ components/Clock.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      time: '',
+      interval: -1
+    }
+  },
+  mounted() {
+    this.time = this.getCurrentTime()
+
+    this.interval = window.setInterval(() => {
+      this.time = this.getCurrentTime()
+    }, 100)
+  },
+  beforeDestroy() {
+    window.clearInterval(this.interval)
+  },
+  methods: {
+    getCurrentTime() {
+      const time = new Date()
+      const sec = time.getSeconds()
+      const fix = sec < 10 ? `0${sec}` : sec.toString()
+      return `${time.getHours()}:${time.getMinutes()}:${fix}`
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="time">{{ time }}</div>
+</template>
+
+<style>
+.time {
+  border: 2px solid;
+  border-radius: 50%;
+}
+</style>
