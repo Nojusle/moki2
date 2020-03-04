7f039799821edc9043f88411babfdbf11a7a3a28
Nojusle <nojusel@gmail.com>
2020-03-04 13:51:21 +0200

    updatee

--- moki2-apollo/src/schema/resolverMap.ts
+++ moki2-apollo/src/schema/resolverMap.ts
@@ -3,9 +3,10 @@ import * as Mongo from "mongodb";
 
 const MONGO_URL = process.env.MONGO_URL || "";
 
-const client = new Mongo.MongoClient(MONGO_URL);
-
-const db = client.db();
+function connect() {
+  const client = new Mongo.MongoClient(MONGO_URL);
+  return client.db();
+}
 
 const messages = ["veikia", MONGO_URL];
 
@@ -19,7 +20,13 @@ const resolverMap: IResolvers = {
     async addMessage(_: void, { msg }: any): Promise<string[]> {
       console.log("msg", msg);
       messages.push(msg);
-      db.collection("messages").insertOne({ msg });
+      try {
+        connect()
+          .collection("messages")
+          .insertOne({ msg });
+      } catch (err) {
+        return [...messages, JSON.stringify(err)];
+      }
       return messages;
     }
   }
