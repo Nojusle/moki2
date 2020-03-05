0dd0e8f2704f57ed13091f5a43aef6634ef16644
Nojusle <nojusel@gmail.com>
2020-03-05 13:46:53 +0200

    fixed auth

--- moki2-apollo/src/schema/resolverMap.ts
+++ moki2-apollo/src/schema/resolverMap.ts
@@ -20,10 +20,11 @@ console.log("MONGO_DB", MONGO_DB);
 console.log("process.env", process.env);
 
 // const URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}?replicaSet=${MONGO_REPLICASET}&authSource=admin`;
+
 const URL =
-  "mongodb://mongo-mongodb-replicaset-0.mongo-mongodb-replicaset.default.svc.cluster.local,mongo-mongodb-replicaset-1.mongo-mongodb-replicaset.default.svc.cluster.local,mongo-mongodb-replicaset-2.mongo-mongodb-replicaset.default.svc.cluster.local:27017/sharkinfo?replicaSet=rs0";
-async function connect(msg: string) {
-  return (await Mongo.MongoClient.connect(msg)).db().collection("messages");
+  "mongodb://bm9qdXM=:bm9qdXMxMjM=@mongo-mongodb-replicaset-0.mongo-mongodb-replicaset.default.svc.cluster.local,mongo-mongodb-replicaset-1.mongo-mongodb-replicaset.default.svc.cluster.local,mongo-mongodb-replicaset-2.mongo-mongodb-replicaset.default.svc.cluster.local:27017/sharkinfo?replicaSet=rs0&authSource=admin";
+async function connect() {
+  return (await Mongo.MongoClient.connect(URL)).db().collection("messages");
 }
 
 async function getMessages(collection: Mongo.Collection) {
@@ -33,12 +34,12 @@ async function getMessages(collection: Mongo.Collection) {
 const resolverMap: IResolvers = {
   Query: {
     async messages(_: void, args: void): Promise<string[]> {
-      return getMessages(await connect(URL));
+      return getMessages(await connect());
     }
   },
   Mutation: {
     async addMessage(_: void, { msg }: any): Promise<string[]> {
-      const connection = await connect(msg);
+      const connection = await connect();
       await connection.insertOne({ msg });
       return getMessages(connection);
     }
